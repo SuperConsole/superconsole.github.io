1986cafae719b4eb72f931acbf20ca350c0541ee
Qzifyes <Qzifyes>
2018-04-18 18:03:51 +0900

    edited

---
+++ assets/js/spcn.jquery.js
@@ -0,0 +1,21 @@
+//jQuery
+//Product by SuperConsole®.
+
+//サブメニューのトグル化
+var toggle = true;
+
+$(function(){
+    $(".side-bar").click(function () {
+        if(toggle){
+            $("#main").fadeOut(500);
+            $(".menu").fadeIn(500);
+            setTimeout($("#main").hide(),500);
+            toggle=false;
+        }else{
+            $("#main").fadeIn(500);
+            $(".menu").fadeOut(500);
+            setTimeout($(".menu").hide(),500);
+            toggle=true;
+        }
+    });
+});
--- assets/js/spcn.react.js
+++ assets/js/spcn.react.js
@@ -1,18 +1,7 @@
 //React.js
 //Product by SuperConsole®.
 
-var TestClass = React.createClass({
-    render: function(){
-        return <h1>{this.props.text}</h1>;
-    }
-});
-
 ReactDOM.render(
-    React.createElement(
-        TestClass,
-        {
-            text:"Home"
-        }
-    ),
-    document.getElementById('top_header')
+    <h1>Home</h1>,
+    document.getElementById('main_header')
 );
